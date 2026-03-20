bfbea05efd84c0ec46c1d7b20f1e3b353e135130
AngeMalta <angemalta147@gmail.com>
2026-03-19 22:29:02 -0300

    aula 2 variaveis e operationes

---
+++ aula02-var-ops/aula02-vars.py
@@ -0,0 +1,25 @@
+from param import String
+print("hello word :D \n")
+'''
+noussa qui legal :D
+us comentaro ta tudo em linha
+diferente meu
+ki legau
+'''
+
+#variaveis mt loca
+
+nome = "AMR"
+idade = 18
+peso = 87.3
+print(nome, idade, peso)
+print(f"oiiiii, comos vós estais meu carinhoso amigo denominado {nome}, desejo-ti uma deveras de oportuna noite para que possais ir ao descanso até o período do nascer do sol, sem interrupções perante vosso momento de descanso")
+
+#inputas
+
+nome = input("Digitai vosso nome pwease\n")
+idade = int(input("vai digita tua idade ae\n"))
+nova_idade = idade +1
+
+print(nome, idade)
+print(nova_idade)
